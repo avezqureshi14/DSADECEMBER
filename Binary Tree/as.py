25999edd9ce7070ccbb7c1dc6b938fc37ebf8a84
ubaidubai <avez3npqureshi@gmail.com>
2022-12-09 10:30:04 +0530

    first

---
+++ Binary Tree/as.py
@@ -0,0 +1,8 @@
+a =int(input("Enter a num1 to find the factorial :\n"))
+f=1
+for i in range (1,a+1):
+    f=f*i
+print(f"The factorial of the given number is {f}")
+
+
+
--- Patterns/p1.py
+++ Patterns/p1.py
@@ -1,4 +1,4 @@
-n=int(input("Entre num1 : \n"))
+n=int(input("Entre number of rows : \n"))
 for i in range (1,n+1) :
   for j in range( 1,i+1) :
     print(i+1 ,end=" ")
